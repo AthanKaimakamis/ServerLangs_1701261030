88d9c51585cae873489a41ed46dfb0e25029b863
AthanKaimakamis <kaim.athanasios@gmail.com>
2022-10-27 08:00:03 +0300

    Starting Template

---
+++ Warehouse.Manager/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Warehouse.Manager.Models
+{
+    public class User
+    {
+        public int SID { get; set; }
+        public string Username { get; set; }
+
+    }
+}
+
---
+++ Warehouse.Manager/Services/DbDataScopedService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+using Warehouse.Manager.Models;
+using Warehouse.Manager.Utils;
+
+namespace Warehouse.Manager.Services
+{
+    public interface IDbDataScopedService
+    {
+        Task<List<User>> GetUser();
+    }
+
+    public class DbDataScopedService : DbCommandBase, IDbDataScopedService
+    {
+        public DbDataScopedService(IOptions<AppConfig> appSettings, ILoggingSingletonService loggingService, IHttpContextAccessor httpContextAccessor) : base(appSettings, loggingService, httpContextAccessor) { }
+
+        public async Task<List<User>> GetUser()
+        {
+            return await base.GetListResult<User>("select u.sid,u.username from [pb].[Users] u");
+        }
+    }
+}
+
